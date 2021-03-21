8f1ebcac6319f02501443bb7b614479f45d65ad9
Jeremy Schiff <jschiff90@gmail.com>
2021-03-20 20:47:55 -0400

    Create MathUtilities.cs

---
+++ Runtime/Utilities/MathUtilities.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public static class MathUtilities {
+    public static float Remap(float input, float oldLow, float oldHigh, float newLow, float newHigh) {
+        float t = Mathf.InverseLerp(oldLow, oldHigh, input):
+        return Mathf.Lerp(newLow, newHigh, t);
+    }
+    
+    public static float Remap(float input, float oldHigh, float newHigh) {
+        return Remap(0, oldHigh, 0, newHigh, input);
+    }
+}
