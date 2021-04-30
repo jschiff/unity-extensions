78e5a5663a9b146e78a275891cf0e6cd14bf7701
Jeremy Schiff <jschiff90@gmail.com>
2021-04-30 15:23:44 -0400

    Update MathUtilities.cs

--- Runtime/Utilities/MathUtilities.cs
+++ Runtime/Utilities/MathUtilities.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class MathUtilities {
     public static float Remap(float input, float oldLow, float oldHigh, float newLow, float newHigh) {
-        float t = Mathf.InverseLerp(oldLow, oldHigh, input):
+        float t = Mathf.InverseLerp(oldLow, oldHigh, input);
         return Mathf.Lerp(newLow, newHigh, t);
     }
     
