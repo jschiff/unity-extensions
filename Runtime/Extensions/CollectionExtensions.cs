f22ed0978b4495a64f5261c1bed6d3f98e8a77d9
Jeremy Schiff <jschiff90@gmail.com>
2021-12-25 18:21:11 -0500

    fix compile error

--- Runtime/Extensions/CollectionExtensions.cs
+++ Runtime/Extensions/CollectionExtensions.cs
@@ -87,7 +87,7 @@ namespace Com.Jschiff.UnityExtensions {
             }
         }
         
-        public static int IndexOf<T>(this Collections.IReadOnlyList<T> self, T elementToFind) {
+        public static int IndexOf<T>(this IReadOnlyList<T> self, T elementToFind) {
             int i = 0;
             foreach(T element in self)
             {
