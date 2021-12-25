d5fa8238ea702ce3e3aae0347e3ef18feb247014
Jeremy Schiff <jschiff90@gmail.com>
2021-12-25 17:13:06 -0500

    Add IndexOf readonly list extension

--- Runtime/Extensions/CollectionExtensions.cs
+++ Runtime/Extensions/CollectionExtensions.cs
@@ -86,5 +86,16 @@ namespace Com.Jschiff.UnityExtensions {
                 list[i] = t;
             }
         }
+        
+        public static int IndexOf<T>(this Collections.IReadOnlyList<T> self, T elementToFind) {
+            int i = 0;
+            foreach(T element in self)
+            {
+                if(Equals(element, elementToFind))
+                    return i;
+                i++;
+            }
+            return -1;
+        }
     }
 }
