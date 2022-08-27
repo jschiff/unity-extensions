338d306ba17f86efa7beea1618ef486132e42720
Jeremy Schiff <jschiff90@gmail.com>
2022-08-27 16:46:31 -0400

    Some more xform stuff

--- Runtime/Extensions/TransformExtensions.cs
+++ Runtime/Extensions/TransformExtensions.cs
@@ -5,5 +5,25 @@ namespace Com.Jschiff.UnityExtensions {
         public static Vector3 Left(this Transform t) => -t.right;
         public static Vector3 Down(this Transform t) => -t.up;
         public static Vector3 Back(this Transform t) => -t.forward;
+
+        public static void PointWorldVectorAt(this Transform t, Vector3 worldVec, Transform target) {
+            PointWorldVectorAt(t, worldVec, target.position);
+        }
+        public static void PointWorldVectorAt(this Transform t, Vector3 worldVec, Vector3 target) {
+            Vector3 dir = target - t.position;
+            Quaternion diff = Quaternion.FromToRotation(worldVec, dir);
+
+            t.rotation *= diff;
+        }
+
+        public static void PointLocalVectorAt(this Transform t, Vector3 localVec, Transform target) {
+            Vector3 worldVec = t.TransformVector(localVec);
+            PointWorldVectorAt(t, worldVec, target.position);
+        }
+
+        public static void PointLocalVectorAt(this Transform t, Vector3 localVec, Vector3 target) {
+            Vector3 worldVec = t.TransformVector(localVec);
+            PointWorldVectorAt(t, worldVec, target);
+        }
     }
 }
